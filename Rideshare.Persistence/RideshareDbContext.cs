2826dd3246b9035143726cbd518134409bbfbf2b
surafel getahun <surafelgetahun01@gmail.com>
2023-06-13 00:46:55 +0300

    fix(Surafel.Imp-RideRequest-CRUD)

--- Rideshare.Persistence/RideshareDbContext.cs
+++ Rideshare.Persistence/RideshareDbContext.cs
@@ -13,19 +13,25 @@ public class RideshareDbContext: IdentityDbContext<User>
     public DbSet<RideRequest> RideRequests{ get; set; }
 
 
-    public RideshareDbContext(DbContextOptions<RideshareDbContext> options)
-        : base(options)
+    public RideshareDbContext(DbContextOptions<RideshareDbContext> options) : base(options)
     {
         AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
         AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
     }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+{
+    builder.UseNpgsql("User ID=postgres;Password=1234;Server=localhost;Port=5432;Database=ridesharedb;Integrated Security=true;Pooling=true;",
+        o => o.UseNetTopologySuite());
+}
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        base.OnModelCreating(modelBuilder);
         modelBuilder.HasPostgresExtension("postgis");
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(RideshareDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
     }
+     
 
     public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
