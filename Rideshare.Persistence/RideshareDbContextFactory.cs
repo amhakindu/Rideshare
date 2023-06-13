8b12bb15e90177c63a9bcf3205d0da50139b8fba
tedblackson <tewodroslm@gmail.com>
2023-06-13 09:51:20 +0300

    add IDesignTimeDbContextFactory

--- Rideshare.Persistence/RideshareDbContextFactory.cs
+++ Rideshare.Persistence/RideshareDbContextFactory.cs
@@ -1,11 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 
 namespace Rideshare.Persistence;
 
-public class RideshareDbContextFactory
+public class RideshareDbContextFactory : IDesignTimeDbContextFactory<RideshareDbContext>
 {
-    public RideshareDbContext CreateDbContext(string[] args)
+    public RideshareDbContext CreateDbContext(string[] args) 
     {
         IConfigurationRoot configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory()+"../Rideshare.WebApi/")
