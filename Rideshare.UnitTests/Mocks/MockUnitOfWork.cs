cccea544670653d388d56210006bbac7fed42cd4
Gizaw Dagne <gizawdagnemogessie@gmail.com>
2023-06-14 23:13:48 +0300

    Fix (Rideshare -Back end) : fix conflict

--- Rideshare.UnitTests/Mocks/MockUnitOfWork.cs
+++ Rideshare.UnitTests/Mocks/MockUnitOfWork.cs
@@ -5,25 +5,22 @@ namespace Rideshare.UnitTests.Mocks;
 
 public class MockUnitOfWork
 {
-	public static class UnitOfWorkMockFactory
+	public static Mock<IUnitOfWork> GetUnitOfWork()
 	{
-		public static Mock<IUnitOfWork> GetUnitOfWork()
-		{
-			var mockUow = new Mock<IUnitOfWork>();
-			var mockTestEntityRepository = MockTestEntityRepository.GetTestEntityRepository();
-			var mockRideRequestRepository = MockRideRequestRepository.GetRideRequestRepository();
-			var mockDriverRepository = MockDriverRepository.GetDriverRepository();
-			var mockRateRepository = MockRateRepository.GetRateRepository();
+		var mockUow = new Mock<IUnitOfWork>();
+		var mockTestEntityRepository = MockTestEntityRepository.GetTestEntityRepository();
+		var mockRideRequestRepository = MockRideRequestRepository.GetRideRequestRepository();
+		var mockDriverRepository = MockDriverRepository.GetDriverRepository();
+		var mockRateRepository = MockRateRepository.GetRateRepository();
 
-			mockUow.Setup(r => r.TestEntityRepository).Returns(mockTestEntityRepository.Object);
-			mockUow.Setup(r => r.RideRequestRepository).Returns(mockRideRequestRepository.Object);
-			mockUow.Setup(r => r.TestEntityRepository).Returns(mockTestEntityRepository.Object);
-			mockUow.Setup(r => r.DriverRepository).Returns(mockDriverRepository.Object);
-			mockUow.Setup(r => r.RateRepository).Returns(mockRateRepository.Object);
-			
-			mockUow.Setup(r => r.Save()).ReturnsAsync(1);
-			
-			return mockUow;
-		}
+		mockUow.Setup(r => r.TestEntityRepository).Returns(mockTestEntityRepository.Object);
+		mockUow.Setup(r => r.RideRequestRepository).Returns(mockRideRequestRepository.Object);
+		mockUow.Setup(r => r.TestEntityRepository).Returns(mockTestEntityRepository.Object);
+		mockUow.Setup(r => r.DriverRepository).Returns(mockDriverRepository.Object);
+		mockUow.Setup(r => r.RateRepository).Returns(mockRateRepository.Object);
+		
+		mockUow.Setup(r => r.Save()).ReturnsAsync(1);
+		
+		return mockUow;
 	}
 }
