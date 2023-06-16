b12e3d9ef9cee49e51ded833ba2f3abfafa371f8
= <amha@a2sv.org>
2023-06-16 00:36:10 -0700

    Implement testing for rideoffer handlers

--- Rideshare.UnitTests/Mocks/MockUnitOfWork.cs
+++ Rideshare.UnitTests/Mocks/MockUnitOfWork.cs
@@ -7,24 +7,26 @@ public class MockUnitOfWork
 {
 	public static Mock<IUnitOfWork> GetUnitOfWork()
 	{
-		var mockUow = new Mock<IUnitOfWork>();
-		var mockTestEntityRepository = MockTestEntityRepository.GetTestEntityRepository();
-		var mockRideRequestRepository = MockRideRequestRepository.GetRideRequestRepository();
-		var mockDriverRepository = MockDriverRepository.GetDriverRepository();
-		var mockRateRepository = MockRateRepository.GetRateRepository();
-        var mockFeebackRepository = MockFeedbackRepository.GetFeedbackRepository();
-        var mockVehicleRepository = MockVehicleRepository.GetVehicleRepository();
+                var mockUow = new Mock<IUnitOfWork>();
+                var mockTestEntityRepository = MockTestEntityRepository.GetTestEntityRepository();
+                var mockRideRequestRepository = MockRideRequestRepository.GetRideRequestRepository();
+                var mockDriverRepository = MockDriverRepository.GetDriverRepository();
+                var mockRateRepository = MockRateRepository.GetRateRepository();
+                var mockFeebackRepository = MockFeedbackRepository.GetFeedbackRepository();
+                var mockVehicleRepository = MockVehicleRepository.GetVehicleRepository();
+                var mockRideOfferRepository = MockRideOfferRepository.GetRideOfferRepository();
 
-        mockUow.Setup(r => r.VehicleRepository).Returns(mockVehicleRepository.Object);
-        mockUow.Setup(r => r.TestEntityRepository).Returns(mockTestEntityRepository.Object);
-        mockUow.Setup(r => r.RideRequestRepository).Returns(mockRideRequestRepository.Object);
-        mockUow.Setup(r => r.TestEntityRepository).Returns(mockTestEntityRepository.Object);
-        mockUow.Setup(r => r.DriverRepository).Returns(mockDriverRepository.Object);
-        mockUow.Setup(r => r.TestEntityRepository).Returns(mockTestEntityRepository.Object);
-        mockUow.Setup(r => r.FeedbackRepository).Returns(mockFeebackRepository.Object);
-		mockUow.Setup(r => r.RateRepository).Returns(mockRateRepository.Object);
-        mockUow.Setup(r => r.Save()).ReturnsAsync(1);
-        
-        return mockUow;
-    }
+                mockUow.Setup(r => r.VehicleRepository).Returns(mockVehicleRepository.Object);
+                mockUow.Setup(r => r.TestEntityRepository).Returns(mockTestEntityRepository.Object);
+                mockUow.Setup(r => r.RideRequestRepository).Returns(mockRideRequestRepository.Object);
+                mockUow.Setup(r => r.TestEntityRepository).Returns(mockTestEntityRepository.Object);
+                mockUow.Setup(r => r.DriverRepository).Returns(mockDriverRepository.Object);
+                mockUow.Setup(r => r.TestEntityRepository).Returns(mockTestEntityRepository.Object);
+                mockUow.Setup(r => r.FeedbackRepository).Returns(mockFeebackRepository.Object);
+                        mockUow.Setup(r => r.RateRepository).Returns(mockRateRepository.Object);
+                mockUow.Setup(r => r.RideOfferRepository).Returns(mockRideOfferRepository.Object);
+                mockUow.Setup(r => r.Save()).ReturnsAsync(1);
+                
+                return mockUow;
+        }
 }
---
+++ Rideshare.UnitTests/RideOffers/UpdateRideOfferCommandHandlerTests.cs
@@ -0,0 +1,157 @@
+using Moq;
+using Xunit;
+using MediatR;
+using Shouldly;
+using AutoMapper;
+using Rideshare.UnitTests.Mocks;
+using Rideshare.Domain.Entities;
+using Rideshare.Application.Profiles;
+using Rideshare.Application.Responses;
+using Rideshare.Application.Exceptions;
+using Rideshare.Application.Common.Dtos.RideOffers;
+using Rideshare.Application.Contracts.Persistence;
+using Rideshare.Application.Features.RideOffers.Commands;
+using Rideshare.Application.Features.testEntitys.CQRS.Handlers;
+
+namespace Rideshare.UnitTests.RideOffers;
+
+public class UpdateRideOfferCommandHandlerTests
+{
+    private readonly IMapper _mapper;
+    private readonly Mock<IUnitOfWork> _mockUow;
+    private readonly UpdateRideOfferCommandHandler _handler;
+
+    public UpdateRideOfferCommandHandlerTests()
+    {
+        _mockUow = MockUnitOfWork.GetUnitOfWork();
+
+        var mapperConfig = new MapperConfiguration(c => { c.AddProfile<MappingProfile>(); });
+
+        _mapper = mapperConfig.CreateMapper();
+        _handler = new UpdateRideOfferCommandHandler(_mockUow.Object, _mapper);
+    }
+
+    [Fact]
+    public async Task ValidRideOfferUpdateTest()
+    {
+        var newVehicleID = 3;
+        var newOrigin = new LocationDto{
+            Longitude=-56.0,
+            Latitude=26.0
+        };
+        var newDestination = new LocationDto{
+            Longitude=-108.0,
+            Latitude=-82.0
+        };
+        var command = new UpdateRideOfferCommand
+        {
+            RideOfferDto = new UpdateRideOfferDto{
+                Id=1,
+                VehicleID = newVehicleID,
+                CurrentLocation = newOrigin,
+                Destination = newDestination
+            }
+        };
+
+        var response = await _handler.Handle(command, CancellationToken.None);
+
+        response.ShouldNotBeNull();
+        response.ShouldBeOfType<BaseResponse<Unit>>();
+        response.Success.ShouldBeTrue();
+
+        RideOffer temp = await _mockUow.Object.RideOfferRepository.Get(id: 1);
+        temp.VehicleID.ShouldBe(newVehicleID);
+        temp.CurrentLocation.X.ShouldBe(newOrigin.Longitude);
+        temp.CurrentLocation.Y.ShouldBe(newOrigin.Latitude);
+
+        temp.Destination.X.ShouldBe(newDestination.Longitude);
+        temp.Destination.Y.ShouldBe(newDestination.Latitude);
+    }
+
+    [Fact]
+    public async Task RideOfferUpdateWithInvalidRideOfferID()
+    {
+        var command = new UpdateRideOfferCommand
+        {
+            RideOfferDto = new UpdateRideOfferDto{  Id=1234 }
+        };
+        await Should.ThrowAsync<NotFoundException>(async () => await _handler.Handle(command, CancellationToken.None));
+    }
+    
+    [Fact]
+    public async Task RideOfferUpdateWithInvalidCurrentLocationLatitude()
+    {
+        var command = new UpdateRideOfferCommand
+        {
+            RideOfferDto = new UpdateRideOfferDto{
+                Id=2,
+                CurrentLocation = new LocationDto{
+                    Longitude=2.0,
+                    Latitude=-123.0
+                }
+            }
+        };
+        await Should.ThrowAsync<ValidationException>(async () => await _handler.Handle(command, CancellationToken.None));
+    }
+
+    [Fact]
+    public async Task RideOfferUpdateWithInvalidCurrentLocationLongitude()
+    {
+        var command = new UpdateRideOfferCommand
+        {
+            RideOfferDto = new UpdateRideOfferDto{
+                Id=2,
+                CurrentLocation = new LocationDto{
+                    Longitude=-234.0,
+                    Latitude=13.0
+                }
+            }
+        };
+        await Should.ThrowAsync<ValidationException>(async () => await _handler.Handle(command, CancellationToken.None));
+    }
+
+    [Fact]
+    public async Task RideOfferUpdateWithInvalidDestinationLatitude()
+    {
+        var command = new UpdateRideOfferCommand
+        {
+            RideOfferDto = new UpdateRideOfferDto{
+                Id=2,
+                Destination = new LocationDto{
+                    Longitude=1.0,
+                    Latitude=-123.0
+                }
+            }
+        };
+        await Should.ThrowAsync<ValidationException>(async () => await _handler.Handle(command, CancellationToken.None));
+    }
+    
+    [Fact]
+    public async Task RideOfferUpdateWithInvalidDestinationLongitude()
+    {
+        var command = new UpdateRideOfferCommand
+        {
+            RideOfferDto = new UpdateRideOfferDto{
+                Id=2,
+                Destination = new LocationDto{
+                    Longitude=234.0,
+                    Latitude=-12.0
+                }
+            }
+        };
+        await Should.ThrowAsync<ValidationException>(async () => await _handler.Handle(command, CancellationToken.None));
+    }
+
+    [Fact]
+    public async Task RideOfferUpdateWithNonExistingVehicle()
+    {
+        var command = new UpdateRideOfferCommand
+        {
+            RideOfferDto = new UpdateRideOfferDto{
+                Id=1,
+                VehicleID = 1000
+            }
+        };
+        await Should.ThrowAsync<NotFoundException>(async () => await _handler.Handle(command, CancellationToken.None));
+    }
+}
