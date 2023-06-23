74db8474e849ae748d5127de43867a35ba0e9ec9
unknown <samuelabatneh20@gmail.com>
2023-06-23 15:05:13 +0300

    add getall feedback with user Id

--- Rideshare.Persistence/Repositories/FeedbackRepository.cs
+++ Rideshare.Persistence/Repositories/FeedbackRepository.cs
@@ -1,4 +1,5 @@
-﻿using Rideshare.Application.Contracts.Persistence;
+﻿using Microsoft.EntityFrameworkCore;
+using Rideshare.Application.Contracts.Persistence;
 using Rideshare.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -10,9 +11,18 @@ namespace Rideshare.Persistence.Repositories
 {
     public class FeedbackRepository: GenericRepository<Feedback>, IFeedbackRepository
     {
+        private readonly RideshareDbContext _dbContext;
         public FeedbackRepository(RideshareDbContext dbContext):base(dbContext)
         {
-            
+            _dbContext = dbContext;
+        }
+        public async Task<IReadOnlyList<Feedback>> GetAllByUserId(string UserId, int pageNumber, int pageSize)
+        {
+            return await _dbContext.Set<Feedback>().AsNoTracking()
+                .Where(p=>p.UserId == UserId)
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
         }
     }
 }
