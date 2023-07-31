7651b0a67dfea69b07ca2247623276ad16af62f8
GizawAAiT <gizawdagnemogessie@gmail.com>
2023-07-31 11:33:03 +0300

    Fix(backend-rideshare): apply the comments for this pr

--- Rideshare.Application/Features/Commuters/Handlers/GetCommuterStatusQueryHandler.cs
+++ Rideshare.Application/Features/Commuters/Handlers/GetCommuterStatusQueryHandler.cs
@@ -2,49 +2,35 @@ using MediatR;
 using Rideshare.Application.Responses;
 using Rideshare.Application.Contracts.Identity;
 using Rideshare.Application.Common.Dtos.Security;
+using AutoMapper;
 
 namespace Rideshare.Application.Features.Commuters.Queries;
 public class GetCommuterStatusQueryHandler : IRequestHandler<GetCommuterStatusQuery, BaseResponse<CommuterStatusDto>>
 {
 	private readonly IUserRepository _userRepository;
+	private readonly IMapper _mapper;
 
-	public GetCommuterStatusQueryHandler(IUserRepository userRepository)
+
+	public GetCommuterStatusQueryHandler(IUserRepository userRepository, IMapper mapper)
 	{
 		_userRepository = userRepository;
+		_mapper = mapper;
+
 	}
 
 	public async Task<BaseResponse<CommuterStatusDto>> Handle(GetCommuterStatusQuery request, CancellationToken cancellationToken)
 	{
-		var commuters = await _userRepository.GetUsersByRoleAsync("Commuter", 1, int.MaxValue);
-		int ActiveCommuters = 0; int IdleCommuters = 0;
-		
-		if (commuters?.Value != null)
-		{
-			
-			foreach (var commuter in commuters.Value)
-			{
-				if (commuter.LastLogin >= DateTime.Now.AddDays(-30))
-				{
-					ActiveCommuters +=1;
-				}
-				else 
-				{
-					IdleCommuters += 1;
-				}
-			};
-		}
-		
 
-		var responseDto = new CommuterStatusDto
-		{
-			ActiveCommuters = ActiveCommuters,
-			IdleCommuters = IdleCommuters
-		};
 		
-		var response = new BaseResponse<CommuterStatusDto>();
-		response.Success = true;
-		response.Message = "Commuters status count fetched Successfully!";
-		response.Value = responseDto;
+		var commuterStatus = await _userRepository.GetCommuterStatusCountAsync();
+
+		var response = new BaseResponse<CommuterStatusDto>
+		{
+			Success = true,
+			Message = "Commuters status count fetched successfully!",
+			Value = commuterStatus
+		};
+
 		return response;
 
 	}
