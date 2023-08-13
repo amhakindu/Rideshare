c943ec940ad918cb15d3d0c0ac63a43351029499
amhakindu <130461352+amhakindu@users.noreply.github.com>
2023-08-13 11:06:23 +0300

    fix program initialization bug

--- Rideshare.Persistence/PersistenceServiceRegistration.cs
+++ Rideshare.Persistence/PersistenceServiceRegistration.cs
@@ -21,42 +21,7 @@ public static class PersistenceServiceRegistration
 
             opt.UseNpgsql(dataSource, o => o.UseNetTopologySuite()
             .EnableRetryOnFailure());
-        });
-
-        // Read the Haversine function script from the file.
-        string functionScript;
-
-        // Combine the provided path segments
-        string scriptFilePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName, "Rideshare.Persistence", "Configurations", "Routines", "HaversineDistance.sql");
-        
-        if (File.Exists(scriptFilePath))
-            functionScript = File.ReadAllText(scriptFilePath);
-        else
-        {
-            throw new FileNotFoundException($"Haversine function script not found at '{scriptFilePath}'.");
-        }
-        
-        // Check and create the Haversine function.
-        using (var connection = new NpgsqlConnection(connectionString))
-        {
-            connection.Open();
-
-            string functionCheckQuery = "SELECT COUNT(*) FROM information_schema.routines WHERE specific_schema = 'public' AND specific_name = 'haversine_distance' AND routine_type = 'FUNCTION'";
-
-            using (var command = new NpgsqlCommand(functionCheckQuery, connection))
-            {
-                long functionExists = (long)command.ExecuteScalar();
-
-                if (functionExists == 0)
-                {
-                    using (var createFunctionCommand = new NpgsqlCommand(functionScript, connection))
-                    {
-                        createFunctionCommand.ExecuteNonQuery();
-                    }
-                    Console.WriteLine("Successfully updated the haversine distance function!");
-                }
-            }
-        }
+        });            
 
         services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddScoped<IUserRepository , UserRepository>();
