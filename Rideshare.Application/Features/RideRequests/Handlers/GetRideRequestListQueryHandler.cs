76470f7be6b9d2e9bdf6a9c22cc75770b244ec4b
surafel getahun <surafelgetahun01@gmail.com>
2023-08-16 16:29:31 +0300

    surafel.RideRequest code refactoring

--- Rideshare.Application/Features/RideRequests/Handlers/GetRideRequestListQueryHandler.cs
+++ Rideshare.Application/Features/RideRequests/Handlers/GetRideRequestListQueryHandler.cs
@@ -22,13 +22,7 @@ public class GetRideRequestListQueryHandler : IRequestHandler<GetRideRequestList
     { 
 
         var response = new PaginatedResponse<RideRequestDto>();
-        Console.WriteLine("ssfssssssssssssssssssssssssssssssss");
         var result = await _unitOfWork.RideRequestRepository.SearchByGivenParameter(request.PageNumber, request.PageSize, request.RideRequestsListFilterDto!.status, request.RideRequestsListFilterDto.fare, request.RideRequestsListFilterDto.name, request.RideRequestsListFilterDto.phoneNumber);
-        Console.WriteLine(result.Value);
-        Console.WriteLine(result.Count);
-
-
-        Console.WriteLine("dsddddddddddddddddddddddddddddddddd");
        
         response.Message = "Fetch Successful";
         response.Value = _mapper.Map<IReadOnlyList<RideRequestDto>>(result.Value);
